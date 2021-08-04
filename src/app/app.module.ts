3c1a3f98c70979232dd452926f1c77bd4fd183c3
Becky <bren@wlgore.com>
2021-08-04 09:10:14 -0400

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
-
+import { CategoryListPipe } from './category-list.pipe'
 @NgModule({
   imports: [
     BrowserModule
